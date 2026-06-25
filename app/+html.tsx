f80a8d5b4705bd835cb05172a61cf1d9d6e3df3a
Joao <joaovitorcavalheiro522@gmail.com>
2026-06-25 20:48:18 -0300

    Improve PWA install metadata

---
+++ app/+html.tsx
@@ -0,0 +1,29 @@
+import { ScrollViewStyleReset } from "expo-router/html";
+import type { PropsWithChildren } from "react";
+
+export default function Html({ children }: PropsWithChildren) {
+  return (
+    <html lang="pt-BR">
+      <head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no, viewport-fit=cover"
+        />
+        <meta name="theme-color" content="#171A20" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <meta name="apple-mobile-web-app-title" content="Oasis Validador" />
+        <meta name="application-name" content="Oasis Validador" />
+        <meta name="description" content="Validador administrativo de ingressos do Clube Oasis." />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="icon" href="/favicon.png" />
+        <link rel="apple-touch-icon" href="/icon-192.png" />
+        <ScrollViewStyleReset />
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
